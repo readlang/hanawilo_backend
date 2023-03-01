a759933623202db99698fc8cee462e820e9a785f
Read Langworthy <rlangworthy@gmail.com>
2023-02-28 18:33:29 -0800

    test commit

---
+++ week1/week1_assignment.js
@@ -0,0 +1,18 @@
+// week 1 assignment
+
+// PROBLEM 1:
+/* 
+2 == '2'    TRUE
+'he' == 'she'   FALSE
+2 === 2     TRUE
+'true' == true  FALSE
+true === true TRUE
+'true' != true  TRUE
+'true' !== true     TRUE
+*/
+
+
+// PROBLEM 2:
+let variable = 0
+const anotherVariable = 1
+var badWayOfDeclaring = 2
