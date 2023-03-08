690586e86be92c0c9cc9e0b6193cef4fe8f7866b
Read Langworthy <rlangworthy@gmail.com>
2023-03-07 21:06:21 -0800

    pausing on class2

---
+++ node_project/controllers/categoryController.js
@@ -0,0 +1,36 @@
+// For '/category' endpoints
+
+const getCategories = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: 'You hit me! Show me all the categories!' })
+}
+
+const createCategory = (req, res, next) => {
+    res
+    .status(201)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: `Create catergory with category name of ${req.body.categoryName} and gender of ${req.body.gender}` })
+}
+
+const putCategory = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: 'You hit me! Show me all the categories!' })
+}
+
+const deleteCategories = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: 'deleting the categories!' })
+}
+
+module.exports = {
+    getCategories,
+    createCategory,
+    putCategory,
+    deleteCategories
+}
---
+++ node_project/server.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const dotenv = require('dotenv');
+const category = require('./routes/category')
+
+dotenv.config({ path: './config/config.env' })
+
+const app = express();
+
+// parse application/json
+app.use(bodyParser.json())
+
+//define your resources
+app.use('/category', category) // route: url/category 
+
+const PORT = process.env.PORT || 5001
+
+const server = app.listen(PORT, () => {
+    console.log(`Awesome server is listening on PORT: ${PORT}`)
+})
+
