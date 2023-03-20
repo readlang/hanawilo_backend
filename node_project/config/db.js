c2d988a9271c8e3fb93626306b9c0c78f8e05333
Read Langworthy <rlangworthy@gmail.com>
2023-03-20 13:53:38 -0700

    fixed error in db.js

--- node_project/config/db.js
+++ node_project/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(config.env.MONGO_URL) // process.env or config.env?
+    const conn = await mongoose.connect(process.env.MONGO_URL)
 
     console.log(conn)
     console.log(`MongoDB Connected: ${conn.connection.host}`)
